5c15179a2eac675a6194ad89a459f942f86e8cbe
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-18 06:54:25 +0000

    updated syntax for test cases

--- dev/0-main.c
+++ dev/0-main.c
@@ -9,21 +9,21 @@ int main(void)
     int len;
     int len2;
 
-    len = _printf("custom     |Let's try to printf a simple sentence.\n");
-    len2 = printf("standard   |Let's try to printf a simple sentence.\n");
-    printf("custom     |Len :[%d, %i]\n", len, len);
-    printf("standard   |Len2:[%d, %i]\n", len2, len2);
-    len = _printf("custom     |testing char 'c': %c\n", 'c');
-    len2 = printf("stdlib     |testing char 'c': %c\n", 'c');
-    printf("custom     |Len :[%d, %i]\n", len, len);
-    printf("standard   |Len2:[%d, %i]\n", len2, len2);
-    len = _printf("custom     |testing: string 'string': %s\n", "string");
-    len2 = printf("stdlib     |testing: string 'string': %s\n", "string");
-    printf("custom     |Len :[%d, %i]\n", len, len);
-    printf("standard   |Len2:[%d, %i]\n", len2, len2);
+	len = _printf("custom     |Let's try to printf a simple sentence.\n");
+	len2 = printf("standard   |Let's try to printf a simple sentence.\n");
+	printf("custom     |Len :[%d, %i]\n", len, len);
+	printf("standard   |Len2:[%d, %i]\n", len2, len2);
+	len = _printf("custom     |testing char 'c': %c\n", 'c');
+	len2 = printf("stdlib     |testing char 'c': %c\n", 'c');
+	printf("custom     |Len :[%d, %i]\n", len, len);
+	printf("standard   |Len2:[%d, %i]\n", len2, len2);
+	len = _printf("custom     |testing: string 'string': %s\n", "string");
+	len2 = printf("stdlib     |testing: string 'string': %s\n", "string");
+	printf("custom     |Len :[%d, %i]\n", len, len);
+	printf("standard   |Len2:[%d, %i]\n", len2, len2);
 	len = _printf("custom     |testing: percent %%\n");
-    len2 = printf("stdlib     |testing: percent %%\n");
-    printf("custom     |Len :[%d, %i]\n", len, len);
-    printf("standard   |Len2:[%d, %i]\n", len2, len2);
+	len2 = printf("stdlib     |testing: percent %%\n");
+	printf("custom     |Len :[%d, %i]\n", len, len);
+	printf("standard   |Len2:[%d, %i]\n", len2, len2);
 	return (0);
 }
