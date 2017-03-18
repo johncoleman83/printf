fa5a6bd5b8562bcd41bfd336ced83914ddb47da7
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-18 07:59:09 +0000

    removed repeat code and added skip check

--- dev/_printf.c
+++ dev/_printf.c
@@ -7,27 +7,23 @@
 int _printf(const char *format, ...)
 {
 	va_list arg_list;
-	unsigned int i = 0, sum_chars = 0;
+	unsigned int i = 0, sum_chars = 0, skip;
 	int (*temp_func)(va_list) = NULL;
 
 	va_start(arg_list, format);
 	while (format && format[i])
 	{
+		skip = 0;
 		if (format[i] == '%')
 		{
 			temp_func = get_directive(format[i + 1]);
 			if (temp_func)
 			{
 				sum_chars += temp_func(arg_list);
-				i += 2;
-			}
-			else
-			{
-				_putchar(format[i++]);
-				sum_chars++;
+				i += 2, skip = 1;
 			}
 		}
-		else
+		if (!skip)
 		{
 			_putchar(format[i++]);
 			sum_chars++;
