81773cd6898a1c66456cf29660fe4e84ba64d519
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-20 00:12:02 +0000

    changed variable skip to no_directive because it's easier to understand

--- dev/_printf.c
+++ dev/_printf.c
@@ -8,23 +8,23 @@ int _printf(const char *format, ...)
 {
 	va_list arg_list;
 	char buffer[1024];
-	unsigned int i = 0, buf_len = 0, skip;
+	unsigned int i = 0, buf_len = 0, no_directive;
 	int (*temp_func)(char *, va_list);
 
 	va_start(arg_list, format);
 	while (format && format[i])
 	{
-		skip = 0;
+		no_directive = 1;
 		if (format[i] == '%')
 		{
 			temp_func = get_directive(format[i + 1]);
 			if (temp_func)
 			{
 				buf_len += temp_func(buffer + buf_len, arg_list);
-				i += 2, skip = 1;
+				i += 2, no_directive = 0;
 			}
 		}
-		if (!skip)
+		if (no_directive)
 		{
 			buffer[buf_len] = format[i++];
 			buf_len++;
